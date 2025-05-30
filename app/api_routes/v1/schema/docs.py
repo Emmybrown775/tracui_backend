6541c9e6fa61b4f0370817785f67914a99b382d8
Emmybrown775 <elumezeemma@gmail.com>
2025-05-30 02:15:41 +0100

    fixed requirement

--- app/api_routes/v1/schema/docs.py
+++ app/api_routes/v1/schema/docs.py
@@ -5,7 +5,7 @@ from app.extensions import api
 
 signup_model = api.model('SignUp', {
     'access_token': fields.String(required=True, description='The user\'s email address'),
-    'access_secret': fields.String(required=True, description='The user\'s email address'),
+    'access_secret': fields.String(description='The user\'s email address'),
     'email': fields.String(required=True, description='The user\'s password'),
     'address': fields.String(required=True, description='The user\'s username'),
     'account_type': fields.String(required=True, description='The user\'s gender'),
--- app/api_routes/v1/schema/forms.py
+++ app/api_routes/v1/schema/forms.py
@@ -19,7 +19,7 @@ def enum_validator(enum_class):
 
 class SignUpForm (FlaskForm):
     access_token = StringField("First Name", validators=[DataRequired()])
-    access_secret = StringField("Last Name", validators=[DataRequired()])
+    access_secret = StringField("Last Name", validators=[])
     email = EmailField("Email", validators=[DataRequired(), Email()])
     account_type = StringField("Gender", validators=[DataRequired(), enum_validator(AccountType)])
     address = StringField("Last Name", validators=[DataRequired()])
